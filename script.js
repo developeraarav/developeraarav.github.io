8b1852587c0a7eb5105b2d7d9d5f8c3b0f98e454
omnitrix21 <okand7946@gmail.com>
2023-07-27 11:08:59 +0530

    re-init type.js

--- script.js
+++ script.js
@@ -1,42 +1,10 @@
-const texts = ["hello there!!", "I'm Aarav Pandey"]; // Array of texts to be typed
-const typeSpeed = 100; // Typing speed in milliseconds
-const eraseSpeed = 50; // Speed to erase each character in milliseconds
-const delayAfterTyping = 1000; // Delay after typing each text in milliseconds
-const delayAfterErasing = 500; // Delay after erasing each text in milliseconds
-
-const nameElement = document.getElementById("name");
-let textIndex = 0;
-let charIndex = 0;
-let isTyping = true;
-let isErasing = false;
-
-function type() {
-  if (charIndex < texts[textIndex].length && isTyping) {
-    nameElement.textContent += texts[textIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typeSpeed);
-  } else if (!isErasing) {
-    isTyping = false;
-    isErasing = true;
-    setTimeout(erase, delayAfterTyping);
-  }
-}
-
-function erase() {
-  if (charIndex > 0 && isErasing) {
-    nameElement.textContent = texts[textIndex].substring(0, charIndex - 1);
-    charIndex--;
-    setTimeout(erase, eraseSpeed);
-  } else if (isErasing) {
-    isTyping = true;
-    isErasing = false;
-    textIndex = (textIndex + 1) % texts.length;
-    setTimeout(type, delayAfterErasing);
-  }
-}
-
-// Start the typing animation when the page loads
-window.onload = function () {
-  type();
-};
-
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  var typed2 = new Typed('#typed2', {
+    strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+    typeSpeed: 50,    // Adjust the typing speed here (higher value means slower speed)
+    backSpeed: 50,    // Adjust the backspacing speed here (higher value means slower speed)
+    fadeOut: true,
+    loop: true
+  });
+});
