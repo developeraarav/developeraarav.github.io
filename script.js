61741b3bb08e330723754dfd5a36adb6250ee3d0
aarav pandey <102042285+developeraarav@users.noreply.github.com>
2023-07-27 09:40:12 +0530

    update and fixup!

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var typed = new Typed('#name', {
-        strings: ['Aarav Pandey'],
-        typeSpeed: 80,
-        startDelay: 1000,
-        showCursor: false
-    });
-});
+// Function to initialize the typing animation
+function initTyped() {
+  const textArray = ["hello there!!", "im aarav pandey"]; // Array of texts to be typed
+  const options = {
+    typeSpeed: 80, // Typing speed in milliseconds
+    loop: false, // Whether to loop through the texts or not
+    showCursor: true, // Whether to display the cursor or not
+    cursorChar: "|", // Custom cursor character
+  };
+
+  // Initialize Typed.js on the <h1> element with ID "name"
+  new Typed("#name", options).typed = textArray;
+}
+
+// Call the function to start the typing animation
+initTyped();
