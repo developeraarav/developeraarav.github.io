61575f0e1a7340130d639af749aee19e7e1f655b
omnitrix21 <okand7946@gmail.com>
2023-09-30 19:32:53 +0530

    website: init type.js
    **type animation

---
+++ type.js
@@ -0,0 +1,13 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    var options = {
+      strings: ["Aarav Pandey"],
+      typeSpeed: 50, // typing speed in milliseconds
+      backSpeed: 25, // backspacing speed in milliseconds
+      loop: false, // whether to loop the animation
+      showCursor: true, // whether to show the blinking cursor
+    };
+
+    var typed = new Typed("#typed-output", options);
+  });
+</script>
