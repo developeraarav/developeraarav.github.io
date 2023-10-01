03d5b1e00908ba6a0cb2b58c3c8f9090aa6c8cdb
omnitrix21 <okand7946@gmail.com>
2023-10-01 09:39:34 +0530

    type.js: looping text

--- type.js
+++ type.js
@@ -1,13 +1,17 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
 <script>
-  document.addEventListener("DOMContentLoaded", function () {
-    var options = {
-      strings: ["Aarav Pandey"],
-      typeSpeed: 50, // typing speed in milliseconds
-      backSpeed: 25, // backspacing speed in milliseconds
-      loop: false, // whether to loop the animation
-      showCursor: true, // whether to show the blinking cursor
-    };
+    document.addEventListener("DOMContentLoaded", function () {
+        var options = {
+            strings: ["Aarav Pandey", "a Developer", "a Learner"],
+            typeSpeed: 70,      // Typing speed in milliseconds
+            backSpeed: 50,      // Backspacing speed in milliseconds
+            startDelay: 0,      // Delay before typing starts in milliseconds
+            backDelay: 1500,    // Delay before backspacing in milliseconds
+            loop: true,         // Set to true to loop the animation
+            showCursor: true,   // Show blinking cursor
+            cursorChar: "|",    // Cursor character
+        };
 
-    var typed = new Typed("#typed-output", options);
-  });
+        var typed = new Typed("#typed-output", options);
+    });
 </script>
